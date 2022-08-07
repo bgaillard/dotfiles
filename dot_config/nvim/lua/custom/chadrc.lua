9cd3b76fa5050824fded7b95188b3e06efe9fc2f
Baptiste Gaillard <baptiste.gaillard@gmail.com>
2022-08-07 20:32:29 +0200

    Improve neovim configuration.

--- dot_config/nvim/lua/custom/chadrc.lua
+++ dot_config/nvim/lua/custom/chadrc.lua
@@ -1,11 +1,147 @@
 local M = {}
 
+
 M.plugins = {
   user = require "custom.plugins",
 
   override = {
 
+    -- lspconfig
+    --
+    -- https://github.com/neovim/nvim-lspconfig
+    ["neovim/nvim-lspconfig"] = {
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = { "require", "vim" },
+          },
+        }
+      }
+    },
+
+    -- mason
+    --
+    -- https://github.com/williamboman/mason.nvim
+    ["williamboman/mason.nvim"] = {
+      ensure_installed = {
+
+        -- DevOps / SRE
+        "ansible-language-server",
+
+        -- Docker
+        "dockerfile-language-server",
+        "hadolint",
+
+        -- Go
+        "delve",
+        "go-debug-adapter",
+        "gofumpt",
+        "goimports",
+        "golangci-lint",
+        "golangci-lint-langserver",
+        "golines",
+        "gomodifytags",
+        "gopls",
+        "gotests",
+        "impl",
+        "revive",
+        "staticcheck",
+
+        -- LaTex
+        "ltex-ls",
+
+        -- Java
+        "clang-format",
+        "jdtls",
+
+        -- Javascript / Typescript
+        "chrome-debug-adapter",
+        "eslint-lsp",
+        "eslint_d",
+        "firefox-debug-adapter",
+        "node-debug2-adapter",
+        "quick-lint-js",
+        "rome",
+        "typescript-language-server",
+        "xo",
+
+        -- JSON
+        "fixjson",
+        "json-lsp",
+        "json-to-struct",
+
+        -- Lua
+        "lua-language-server",
+        "luacheck",
+        "luaformatter",
+        "stylua",
+
+        -- Markdown / Text
+        "alex",
+        "grammarly-languageserver",
+        "markdownlint",
+        "marksman",
+        "proselint",
+        "prosemd-lsp",
+        "remark-language-server",
+        "vale",
+        "write-good",
+
+        -- Python
+        "black",
+        "debugpy",
+        "flake8",
+        "isort",
+        "mypy",
+        "pylint",
+        "python-lsp-server",
+        "sourcery",
+
+        -- Shell
+        "awk-language-server",
+        "bash-language-server",
+        "shellcheck",
+        "shellharden",
+        "shfmt",
+
+        -- SQL
+        "sql-formatter",
+        "sqlfluff",
+        "sqlls",
+        "sqls",
+
+        -- Terraform
+        "terraform-ls",
+        "tflint",
+
+        -- TOML
+        "taplo",
+
+        -- YAML
+        "yaml-language-server",
+        "yamllint",
+
+        -- Web
+        "css-lsp",
+        "cssmodules-language-server",
+        "djlint",
+        "tailwindcss-language-server",
+        "html-lsp",
+
+        -- Others
+        -- "diagnosticsls",
+        "jq",
+        -- "prettier",
+        -- "prettierd",
+        -- "stylelint_lsp",
+        -- "vimls",
+
+      }
+    },
+
     -- nvim-tree
+    --
+    -- https://github.com/kyazdani42/nvim-tree.lua
     ["kyazdani42/nvim-tree.lua"] = {
       git = {
         enable = true,
@@ -17,13 +153,48 @@ M.plugins = {
           }
         }
       }
-    }
+    },
+
+    -- treesitter
+    --
+    -- https://github.com/nvim-treesitter/nvim-treesitter
+    ["nvim-treesitter/nvim-treesitter"] = {
+
+      -- Required by spellsitter
+      additional_vim_regex_highlighting = false,
+
+      ensure_installed = {
+        "c",
+        "cpp",
+        "css",
+        "dockerfile",
+        "hcl",
+        "go",
+        "html",
+        "java",
+        "javascript",
+        "json",
+        "latex",
+        "make",
+        "ninja",
+        "php",
+        "python",
+        "rust",
+        "scss",
+        "sql",
+        "toml",
+        "typescript",
+        "vim",
+        "yaml",
+      }
+    },
 
   }
 }
 
 M.ui = {
-  theme = "gatekeeper"
+  theme = "gatekeeper",
+  transparency = true
 }
 
 return M
--- dot_config/nvim/lua/custom/plugins/init.lua
+++ dot_config/nvim/lua/custom/plugins/init.lua
@@ -1,5 +1,121 @@
+-- https://alpha2phi.medium.com/learn-neovim-the-practical-way-8818fcf4830f
+-- https://github.com/rockerBOO/awesome-neovim
+
 return {
-  ["neoclide/coc.nvim"] = {
-    branch = "release"
-  }
+
+  -- auto-save
+  --
+  -- https://github.com/Pocco81/auto-save.nvim
+  ["Pocco81/auto-save.nvim"] = {
+    config = function()
+      require("auto-save").setup {
+        -- your config goes here
+        -- or just leave it empty :)
+      }
+    end,
+  },
+
+  -- diffview.nvim
+  --
+  -- https://github.com/sindrets/diffview.nvim
+  ["sindrets/diffview.nvim"] = {
+    requires = { "nvim-lua/plenary.nvim" },
+    after = "plenary.nvim",
+    opt = false,
+    config = function()
+      require("diffview").setup {}
+    end,
+  },
+
+  -- FixCursorHold.nvim
+  --
+  -- https://github.com/antoinemadec/FixCursorHold.nvim
+  ["antoinemadec/FixCursorHold.nvim"] = {},
+
+  -- lspconfig
+  --
+  -- https://github.com/neovim/nvim-lspconfig
+  ["neovim/nvim-lspconfig"] = {
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.plugins.lspconfig"
+    end,
+  },
+
+  -- neotest
+  --
+  -- https://github.com/nvim-neotest/neotest
+  ["nvim-neotest/neotest"] = {
+    requires = {
+
+      -- required dependencies
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "antoinemadec/FixCursorHold.nvim",
+
+      -- adapters
+      "nvim-neotest/neotest-python",
+      "nvim-neotest/neotest-go",
+      "rouge8/neotest-rust",
+    },
+    config = function()
+      require("neotest").setup({
+        adapters = {
+          require("neotest-python"),
+          require("neotest-go"),
+          require("neotest-rust"),
+        }
+      })
+    end,
+  },
+  ["nvim-neotest/neotest-python"] = {},
+  ["nvim-neotest/neotest-go"] = {},
+  ["rouge8/neotest-rust"] = {},
+
+  -- null-ls
+  --
+  -- https://github.com/jose-elias-alvarez/null-ls.nvim
+  ["jose-elias-alvarez/null-ls.nvim"] = {
+    after = "nvim-lspconfig",
+    config = function()
+      require "custom.plugins.null-ls"
+    end,
+  },
+
+  -- spellsitter
+  --
+  -- https://github.com/lewis6991/spellsitter.nvim
+  ["lewis6991/spellsitter.nvim"] = {
+    config = function()
+      require('spellsitter').setup()
+    end
+  },
+
+  -- tmux.nvim
+  --
+  -- https://github.com/aserowy/tmux.nvim
+  ["aserowy/tmux.nvim"] = {
+    config = function()
+      require('tmux').setup({
+        copy_sync = {
+          enable = true
+        }
+      })
+    end
+
+  },
+
+  -- trouble
+  --
+  -- https://github.com/folke/trouble.nvim
+  ["folke/trouble.nvim"] = {
+    after = "nvim-web-devicons",
+    config = function()
+      require("trouble").setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    end
+  },
 }
