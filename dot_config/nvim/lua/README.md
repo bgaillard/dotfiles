bff92d33f8ef47b5ddc7a72008607ff18b361c66
Baptiste Gaillard <baptiste.gaillard@gmail.com>
2025-09-11 23:26:35 +0200

    Use Mistral.ai in CopilotChat.nvim.

--- dot_config/nvim/lua/README.md
+++ dot_config/nvim/lua/README.md
@@ -2,7 +2,7 @@
 
 ## Updates
 
-- `:NvChadUpdate`
+- `:Lazy update`
 - `:Mason` and then `U`
 - `:TSUpdate`
 
@@ -10,31 +10,10 @@
 
 Display shortcuts with `:NvCheatsheet` or `:Telescope keymaps`.
 
-Edit shortcuts in `lua/core/custom/mappings.lua`, core mappings defined in NvChad and which can be overridden are available in `lua/core/mappings.lua`.
+Edit shortcuts in `mappings.lua`, core mappings defined in NvChad and which can be overridden are available in `lua/core/mappings.lua`.
 
 ## Plugins
 
-Plugins are defined in 2 locations.
+Configuration of NvChad integrated plugins in `lua/configs/*.lua`.
 
-- `lua/core/custom/plugins.lua` for plugins having very simple configurations ;
-- `lua/core/custom/plugins/` for plugins having more complex configurations.
-
-Curently installed plugins are the following.
-
-- [Pocco81/auto-save.nvim](https://github.com/Pocco81/auto-save.nvim)
-- [sindrets/diffview.nvim](https://github.com/sindrets/diffview.nvim)
-- [tpope/vim-fugitive](https://github.com/tpope/vim-fugitive)
-- [nvim-neotest/neotest](https://github.com/nvim-neotest/neotest)
-- [AckslD/nvim-neoclip.lua](https://github.com/AckslD/nvim-neoclip.lua)
-- [tpope/vim-surround](https://github.com/tpope/vim-surround)
-- [aserowy/tmux.nvim](https://github.com/aserowy/tmux.nvim)
-- [folke/tokyonight.nvim](folke/tokyonight.nvim)
-- [folke/trouble.nvim](https://github.com/folke/trouble.nvim)
-- [neovim/nvim-lspconfig](https://github.com/neovim/nvim-lspconfig)
-- [williamboman/mason.nvim](https://github.com/williamboman/mason.nvim)
-- [jose-elias-alvarez/null-ls.nvim](https://github.com/jose-elias-alvarez/null-ls.nvim)
-- [nvim-tree/nvim-tree.lua](https://github.com/nvim-tree/nvim-tree.lua)
-- [nvim-telescope/telescope.nvim](https://github.com/nvim-telescope/telescope.nvim)
-- [xiyaowong/telescope-emoji.nvim]()
-- [nvim-telescope/telescope-fzf-native.nvim](https://github.com/nvim-telescope/telescope-fzf-native.nvim)
-- [nvim-treesitter/nvim-treesitter](https://github.com/nvim-treesitter/nvim-treesitter)
+New plugins in `lua/plugins/*.lua`.
--- dot_config/nvim/lua/plugins/copilot-chat.lua
+++ dot_config/nvim/lua/plugins/copilot-chat.lua
@@ -1,13 +1,62 @@
- -- CopilotChat.nvim
- --
- -- https://github.com/CopilotC-Nvim/CopilotChat.nvim
- return {
-   "CopilotC-Nvim/CopilotChat.nvim",
-   lazy = false,
-   dependencies = {
-     {"github/copilot.vim"},
-     {"nvim-telescope/telescope.nvim", branch = "master"},
-   },
-   build = "make tiktoken",
-   opts = {},
- }
+-- CopilotChat.nvim
+--
+-- https://github.com/CopilotC-Nvim/CopilotChat.nvim
+return {
+  "CopilotC-Nvim/CopilotChat.nvim",
+  lazy = false,
+  dependencies = {
+    {"github/copilot.vim"},
+    {"nvim-telescope/telescope.nvim", branch = "master"},
+  },
+  build = "make tiktoken",
+  opts = {},
+  config = function()
+    require("CopilotChat").setup()
+
+    -- @see https://github.com/CopilotC-Nvim/CopilotChat.nvim/discussions/975
+    require('CopilotChat.config').providers.mistral = {
+      prepare_input = require('CopilotChat.config.providers').copilot.prepare_input,
+      prepare_output = require('CopilotChat.config.providers').copilot.prepare_output,
+
+      get_headers = function()
+        local api_key_file = io.open(os.getenv("HOME") .. '/.mistral_api_key', 'r')
+        if not api_key_file then
+          error('Mistral API key file not found at ~/.mistral_api_key')
+        end
+        local api_key = api_key_file:read()
+        api_key_file:close()
+        return {
+          Authorization = 'Bearer ' .. api_key,
+          ['Content-Type'] = 'application/json',
+        }
+      end,
+
+      get_models = function(headers)
+        local response, err = require('CopilotChat.utils.curl').get('https://api.mistral.ai/v1/models', {
+          headers = headers,
+          json_response = true,
+        })
+
+        if err then
+          error(err)
+        end
+
+        return vim.iter(response.body.data)
+          :filter(function(model)
+            return model.capabilities.completion_chat
+          end)
+          :map(function(model)
+            return {
+              id = model.id,
+              name = model.name,
+            }
+          end)
+          :totable()
+      end,
+
+      get_url = function()
+        return 'https://api.mistral.ai/v1/chat/completions'
+      end,
+    }
+  end,
+}
