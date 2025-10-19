738286d72c408fb4be99b18b0448a85b0eaddb91
Baptiste Gaillard <baptiste.gaillard@gmail.com>
2025-10-19 15:47:32 +0200

    Improve usage of NvimTree and Terminal.

--- dot_config/nvim/lua/plugins/null-ls.lua
+++ dot_config/nvim/lua/plugins/null-ls.lua
@@ -1,6 +1,6 @@
 -- null-ls
- --
- -- https://github.com/jose-elias-alvarez/null-ls.nvim
+--
+-- https://github.com/jose-elias-alvarez/null-ls.nvim
 return {
   {
     "jose-elias-alvarez/null-ls.nvim",
